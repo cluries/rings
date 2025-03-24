0c7c5b76e4823b79ad74055d33d4447744f9c663
cluries <cluries@me.com>
2025-03-24 11:53:00 +0800

    PAF: 2025-03-24 11:53:00

--- src/any.rs
+++ src/any.rs
@@ -1,6 +1,19 @@
 /// Any trait implementation for any type.
 
 /// Any trait implementation for any type.
+///
+/// Defaults implementaion
+///
+/// impl AnyTrait for XXXX {
+///     fn as_any(&self) -> &dyn std::any::Any {
+///         self
+///     }
+///
+///     fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+///         self
+///     }
+/// }
+///
 pub trait AnyTrait: std::any::Any {
     fn as_any(&self) -> &dyn std::any::Any;
     fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
@@ -15,3 +28,4 @@ pub trait AnyTraitImplMut<T>: AnyTrait {
 }
 
 pub trait AnyTraitImpl<T>: AnyTraitImplRef<T> + AnyTraitImplMut<T> {}
+
