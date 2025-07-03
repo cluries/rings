fe834a2b927bd43c494685c0109d4bf6fce6532b
cluries <cluries@me.com>
2025-07-03 15:43:46 +0800

    PAF: 2025-07-03 15:43:46

--- src/tools/promptor.rs
+++ src/tools/promptor.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use std::fmt;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Prompt {
@@ -9,264 +8,173 @@ pub struct Prompt {
 }
 
 pub struct PromptBuilder {
-    skills: Option<Skills>,
-    actions: Option<Actions>,
-    constrains: Option<Constrains>,
-    output_format: Option<OutFormat>,
+    role: Option<String>,
+    skills: Option<Vec<String>>,
+    task: Option<String>,
+    constraints: Option<std::collections::HashMap<String, Vec<String>>>,
+    fewshots: Option<Vec<Fewshot>>,
 }
 
-/// A structured prompt component.
-trait PromptComponent {
-    fn render(&self, w: &mut fmt::Formatter<'_>) -> fmt::Result;
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Fewshot {
+    input: String,
+    output: String,
 }
 
-pub struct CommonComponent {
-    kind: String,
-    value: Vec<String>,
+// pub type Fewshots = Vec<Fewshot>;
+
+pub const CONSTRAINT_GROUP_GLOBAL: &'static str = "Global";
+pub const CONSTRAINT_GROUP_OUTPUT: &'static str = "Output Format";
+
+pub const CONSTRAINT_ONLY_RESULTS_NO_FORMATS: [&'static str; 2] = [
+    "Strictly output only the result, without any prefixes, suffixes, explanations, or formatting. Provide the answer directly.",
+    "严格只输出结果，无需任何前缀、后缀、解释或格式化,直接给出答案即可。",
+];
+
+/// Return JSON in this exact structure
+pub const CONSTRAINT_ONLY_RESULTS_USE_JSON_PREFIX: [&'static str; 2] = [
+    "Strictly return the result in JSON format only, without any extra explanations, notes, or text. Output valid JSON exclusively, like",
+    "请严格以JSON格式返回结果，不要包含任何额外的解释、说明或文本。仅输出有效的JSON内容，格式如下",
+];
+
+pub const CCONSTRAINT_ACCURACY_OVER_CREATIVITY: [&'static str; 2] =
+    ["Prioritize accuracy over creativity in all responses", "所有回答保持中立，不表达主观观点"];
+
+pub fn constraint_json(structure: &str) -> String {
+    let s = structure.split("\n").into_iter().map(|x| x.trim().to_string()).collect::<Vec<String>>();
+    format!("{}: {}", CONSTRAINT_ONLY_RESULTS_USE_JSON_PREFIX[0], s.join(""))
 }
-
-#[derive(Clone, Eq, PartialEq, Debug)]
-pub enum ValueKind {
-    String,
-    Integer,
-    Float,
-    Object,
-    Array,
-    Boolean,
-}
-
-pub struct OutFormatJson {
-    kind: ValueKind,
-    field: String,
-    comment: String,
-    nest_object: Option<Vec<Box<OutFormatJson>>>,
-    nest_array: Option<Box<OutFormatJson>>,
-}
-
-pub struct Skills(Vec<String>);
-pub struct Actions(Vec<String>);
-pub struct Constrains(Vec<String>);
-pub struct OutFormat(OutFormatJson);
-
-pub struct Input(String);
 
 impl Prompt {
     pub fn system(&self) -> &str {
         self.system.as_deref().unwrap_or("")
     }
+
     pub fn assistant(&self) -> &str {
         self.assistant.as_deref().unwrap_or("")
     }
+
     pub fn user(&self) -> &str {
         &self.user
     }
 }
 
 impl PromptBuilder {
-    pub fn new() -> PromptBuilder {
-        PromptBuilder { skills: None, actions: None, constrains: None, output_format: None }
+    pub fn new(role: &str, task: &str) -> PromptBuilder {
+        PromptBuilder { role: Some(role.to_string()), skills: None, task: Some(task.to_string()), constraints: None, fewshots: None }
+    }
+
+    pub fn set_role(mut self, role: &str) -> Self {
+        self.role = Some(role.to_string());
+        self
     }
 
     pub fn add_skill(mut self, skill: &str) -> Self {
         match self.skills.as_mut() {
             None => {
-                self.skills = Some(Skills(vec![String::from(skill)]));
+                self.skills = Some(vec![String::from(skill)]);
             },
             Some(skills) => {
-                skills.0.push(String::from(skill));
+                skills.push(String::from(skill));
             },
         }
         self
     }
 
-    pub fn add_action(mut self, action: &str) -> Self {
-        match self.actions.as_mut() {
+    pub fn set_task(mut self, task: &str) -> Self {
+        self.task = Some(String::from(task));
+        self
+    }
+
+    pub fn add_constraint(mut self, group: &str, constraint: &str) -> Self {
+        if let None = self.constraints {
+            let constrains = std::collections::HashMap::<String, Vec<String>>::new();
+            self.constraints = Some(constrains);
+        }
+
+        let constrains = self.constraints.as_mut().unwrap();
+        match constrains.get_mut(group) {
             None => {
-                self.actions = Some(Actions(vec![String::from(action)]));
+                constrains.insert(String::from(group), vec![String::from(constraint)]);
             },
-            Some(actions) => {
-                actions.0.push(String::from(action));
+            Some(consts) => {
+                consts.push(String::from(constraint));
             },
         }
+
         self
     }
 
-    pub fn add_constraint(mut self, constraint: &str) -> Self {
-        match self.constrains.as_mut() {
-            None => {
-                self.constrains = Some(Constrains(vec![String::from(constraint)]));
-            },
-            Some(constrains) => {
-                constrains.0.push(String::from(constraint));
-            },
-        }
+    pub fn add_constraint_global(self, constraint: &str) -> Self {
+        self.add_constraint(CONSTRAINT_GROUP_GLOBAL, constraint)
+    }
+
+    pub fn add_constraint_output(self, constraint: &str) -> Self {
+        self.add_constraint(CONSTRAINT_GROUP_OUTPUT, constraint)
+    }
+
+    pub fn add_fewshot(mut self, fewshot: Fewshot) -> Self {
+        match self.fewshots.as_mut() {
+            None => self.fewshots = Some(vec![fewshot]),
+            Some(fewshots) => fewshots.push(fewshot),
+        };
         self
     }
 
-    // pub fn set_output_format(mut self, output: OutFormat) -> Self {
-    //     match self.output_format.as_mut() {
-    //         None => {
-    //             self.output_format = Some(OutFormat(output.to_string()));
-    //         },
-    //         Some(format) => {
-    //             format.0 = output.to_string();
-    //         },
-    //     }
-    //     self
-    // }
+    pub fn add_fewshot_directly(self, input: &str, output: &str) -> Self {
+        self.add_fewshot(Fewshot { input: input.into(), output: output.into() })
+    }
 
     pub fn build(self) -> String {
         let mut prompt: Vec<String> = Vec::new();
+        if let Some(role) = self.role {
+            prompt.push(format!("## Role\n- {}", role));
+        }
+
         if let Some(skills) = self.skills {
-            let c: CommonComponent = skills.into();
-            prompt.push(c.to_string());
+            let mut vec: Vec<String> = Vec::new();
+            vec.push("## Skills".to_string());
+            for skill in skills {
+                vec.push(format!("- {}", skill));
+            }
+            prompt.push(vec.join("\n"));
         }
-        if let Some(actions) = self.actions {
-            let c: CommonComponent = actions.into();
-            prompt.push(c.to_string());
-        }
-        if let Some(constraints) = self.constrains {
-            let c: CommonComponent = constraints.into();
-            prompt.push(c.to_string());
+        if let Some(task) = self.task {
+            prompt.push(format!("## Task\n- {}", task));
         }
 
-        prompt.join("\n")
-    }
-}
+        if let Some(constraints) = self.constraints {
+            let mut vec: Vec<String> = Vec::new();
+            vec.push("## Constraints".to_string());
+            let mut keys: Vec<String> = constraints.keys().map(|x| x.to_string()).collect();
+            keys.sort();
 
-impl fmt::Display for CommonComponent {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.render(f)
-    }
-}
+            for (i, group) in keys.iter().enumerate() {
+                vec.push(format!("### {}", group));
+                let constraints = constraints.get(group).unwrap();
+                for constraint in constraints {
+                    vec.push(format!("- {}", constraint));
+                }
+                if i == 0 {
+                    vec.push(" ".to_string());
+                }
+            }
 
-impl PromptComponent for CommonComponent {
-    fn render(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let value = self.value.iter().map(|x| format!("- {}", x)).collect::<Vec<String>>().join("\n");
-        writeln!(f, "## {}\n{}", self.kind, value)
-    }
-}
-
-impl Into<CommonComponent> for Skills {
-    fn into(self) -> CommonComponent {
-        CommonComponent { kind: "Skills".to_string(), value: self.0 }
-    }
-}
-
-impl Into<CommonComponent> for Actions {
-    fn into(self) -> CommonComponent {
-        CommonComponent { kind: "Actions".to_string(), value: self.0 }
-    }
-}
-
-impl Into<CommonComponent> for Constrains {
-    fn into(self) -> CommonComponent {
-        CommonComponent { kind: "Constrains".to_string(), value: self.0 }
-    }
-}
-
-// impl Into<CommonComponent> for OutFormat {
-//     fn into(self) -> CommonComponent {
-//         CommonComponent { kind: "Format".to_string(), value: vec![self.0] }
-//     }
-// }
-
-impl Into<CommonComponent> for Input {
-    fn into(self) -> CommonComponent {
-        CommonComponent { kind: "Input".to_string(), value: vec![self.0] }
-    }
-}
-
-impl OutFormatJson {
-    pub fn prefix() -> &'static str {
-        "Return JSON in this exact structure"
-    }
-
-    pub fn new_str(field: &str, comment: &str) -> OutFormatJson {
-        OutFormatJson {
-            kind: ValueKind::String,
-            field: field.to_string(),
-            comment: comment.to_string(),
-            nest_array: None,
-            nest_object: None,
+            prompt.push(vec.join("\n"));
         }
-    }
 
-    pub fn new_int(field: &str, comment: &str) -> OutFormatJson {
-        OutFormatJson {
-            kind: ValueKind::Integer,
-            field: field.to_string(),
-            comment: comment.to_string(),
-            nest_array: None,
-            nest_object: None,
-        }
-    }
+        if let Some(fewshots) = self.fewshots {
+            let mut vec: Vec<String> = Vec::new();
+            vec.push("## Examples".to_string());
+            for fewshot in fewshots {
+                vec.push(format!("- Input: {}", fewshot.input));
+                vec.push(format!("  Output: {}", fewshot.output));
+            }
 
-    pub fn new_float(field: &str, comment: &str) -> OutFormatJson {
-        OutFormatJson {
-            kind: ValueKind::Float,
-            field: field.to_string(),
-            comment: comment.to_string(),
-            nest_array: None,
-            nest_object: None,
+            prompt.push(vec.join("\n"));
         }
-    }
 
-    pub fn new_bool(field: &str, comment: &str) -> OutFormatJson {
-        OutFormatJson {
-            kind: ValueKind::Boolean,
-            field: field.to_string(),
-            comment: comment.to_string(),
-            nest_array: None,
-            nest_object: None,
-        }
-    }
-
-    pub fn new_array(field: &str, comment: &str, nest_array: OutFormatJson) -> OutFormatJson {
-        OutFormatJson {
-            kind: ValueKind::Array,
-            field: field.to_string(),
-            comment: comment.to_string(),
-            nest_array: Some(Box::new(nest_array)),
-            nest_object: None,
-        }
-    }
-
-    pub fn new_object(field: &str, comment: &str, nest_object: Vec<Box<OutFormatJson>>) -> OutFormatJson {
-        OutFormatJson {
-            kind: ValueKind::Array,
-            field: field.to_string(),
-            comment: comment.to_string(),
-            nest_array: None,
-            nest_object: Some(nest_object),
-        }
-    }
-
-    pub fn description(&self) -> String {
-        let kind = self.kind.clone();
-        match kind {
-            ValueKind::String => {
-                format!("\"{}\": \"string, {}\"", self.field, self.comment)
-            },
-            ValueKind::Integer => {
-                format!("\"{}\": \"integer, {}\"", self.field, self.comment)
-            },
-            ValueKind::Float => {
-                format!("\"{}\": \"float, {}\"", self.field, self.comment)
-            },
-            ValueKind::Object => {
-                let n = self.nest_object.as_ref().unwrap();
-                let n = n.iter().map(|x| x.description()).collect::<Vec<String>>().join(",\n ");
-                format!("\"{}\": {{ {} }}", self.field, n)
-            },
-            ValueKind::Array => {
-                let n = self.nest_array.as_ref().unwrap().description();
-                format!("\"{}\": [ {} ] ", self.field, n)
-            },
-            ValueKind::Boolean => {
-                format!("\"{}\": \"boolean, {}\"", self.field, self.comment)
-            },
-        }
+        prompt.join("\n\n")
     }
 }
 
@@ -276,22 +184,45 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let s = PromptBuilder::new()
-            .add_action("action")
-            .add_constraint("dont_care")
-            .add_constraint("use chinese")
-            .add_skill("convert json to xml")
-            .add_skill("convert xml to json");
+        struct Person {
+            name: String,
+            age: i32,
+        }
+
+        struct Record {
+            time: String,
+            unit: String,
+            amount: i32,
+        }
+
+        struct MoneyMaker {
+            id: i64,
+            person: Person,
+            total: i32,
+            records: Vec<Record>,
+        }
+
+        let s = PromptBuilder::new("monery maker", "make money for people")
+            .add_constraint_global("money is good")
+            .add_constraint_global("use chinese")
+            .add_skill("generate money")
+            .add_skill("generate person")
+            .add_constraint_output(&constraint_json(
+                r#"{
+                    "name": "string, 名字",
+                    "age":  "number, 年龄",
+                    "girls": [
+                        "name": "string, 名字",
+                         "age":  "number, 年龄"
+                    ]
+                }"#,
+            ))
+            .add_fewshot_directly("古大", "天神")
+            .add_fewshot_directly("古大大", "天神");
+
         println!("{}", s.build());
     }
 
     #[test]
-    fn test_json_output_format() {
-        let k = OutFormatJson::new_object(
-            "foo",
-            "bar",
-            vec![Box::new(OutFormatJson::new_int("foo", "bar")), Box::new(OutFormatJson::new_float("foo", "bar"))],
-        );
-        println!("{}", k.description());
-    }
+    fn test_json_output_format() {}
 }
