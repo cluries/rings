07537e4d9e7bb1348a2f9febc27901b1f672076e
cluries <cluries@me.com>
2025-07-12 23:40:40 +0800

    PAF: 2025-07-12 23:40:40

--- src/tools/mcp.rs
+++ src/tools/mcp.rs
@@ -1,1 +1,7 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MCP {
+    pub name: String, 
+}
+ 
--- src/web/input.rs
+++ src/web/input.rs
@@ -14,4 +14,6 @@ impl Inputs {
             },
         }
     }
+
+     
 }
