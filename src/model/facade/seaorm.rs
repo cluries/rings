b27cf82215e8f3eeabf79027fc086d9f90418b07
cluries <cluries@me.com>
2025-09-04 17:02:55 +0800

    Changes: M  AGENT.md M  src/model/facade/seaorm.rs

--- src/model/facade/seaorm.rs
+++ src/model/facade/seaorm.rs
@@ -1,17 +1,1 @@
-pub enum Operation {
-    Create,
-    Read,
-    Update,
-    Delete,
-}
-
-impl Operation {
-    pub fn as_str(&self) -> &'static str {
-        match self {
-            Operation::Create => "create",
-            Operation::Read => "read",
-            Operation::Update => "update",
-            Operation::Delete => "delete",
-        }
-    }
-}
+use sea_orm::{ActiveModelTrait, ColumnTrait, DbErr, EntityTrait, ModelTrait, NotSet, QueryFilter, QueryOrder, Set};
